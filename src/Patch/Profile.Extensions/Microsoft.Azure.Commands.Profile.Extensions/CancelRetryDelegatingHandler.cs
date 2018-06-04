449e609bdb77505636c0eae2ab48a71be11e155e
markcowl <markcowl@microsoft.com>
2018-06-04 15:17:55 -0700

    Adding patch for TasckCanceled Exceptions

---
+++ src/Patch/Profile.Extensions/Microsoft.Azure.Commands.Profile.Extensions/CancelRetryDelegatingHandler.cs
@@ -0,0 +1,69 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.Profile.Extensions
+{
+    /// <summary>
+    /// Class to enable automatic retry of TaskCanceledExsceptions.
+    /// Note that this follows the calling pattern in PowerShell, which does not pass cancellation tokens from
+    /// the base cmdlet
+    /// </summary>
+    public class CancelRetryDelegatingHandler : DelegatingHandler, ICloneable
+    {
+        const int DefaultWaitSeconds = 10;
+        public CancelRetryDelegatingHandler()
+        {
+            WaitInterval = TimeSpan.FromSeconds(DefaultWaitSeconds);
+        }
+        public CancelRetryDelegatingHandler(TimeSpan waitInterval, int maxTries)
+        {
+            WaitInterval = waitInterval;
+            MaxTries = maxTries;
+        }
+
+        public TimeSpan WaitInterval { get; set; } = TimeSpan.FromSeconds(DefaultWaitSeconds);
+
+        public int MaxTries { get; set; } = 3;
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            int tries = 0;
+            do
+            {
+                using (var source = new CancellationTokenSource())
+                {
+                    try
+                    {
+                        return await base.SendAsync(request, source.Token).ConfigureAwait(false);
+                    }
+                    catch (TaskCanceledException) when (tries++ < MaxTries)
+                    {
+                        Thread.Sleep(WaitInterval);
+                    }
+                }
+            }
+            while (true);
+        }
+
+        public object Clone()
+        {
+            return new CancelRetryDelegatingHandler(WaitInterval, MaxTries);
+        }
+    }
+}
---
+++ src/Patch/Profile.Extensions/Microsoft.Azure.Commands.Profile.Extensions/Install-CancelPatch.ps1
@@ -0,0 +1,19 @@
+﻿#  ----------------------------------------------------------------------------------
+# 
+#  Copyright Microsoft Corporation
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http:# www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ----------------------------------------------------------------------------------
+
+#Requires -Modules @{ModuleName="AzureRM.Profile"; ModuleVersion="4.0.0"}
+$dllPath = Join-Path $PSScriptRoot -ChildPath "Microsoft.Azure.Commands.Profile.Extensions.dll"
+Add-Type -Path $dllPath
+$handler = New-Object -TypeName Microsoft.Azure.Commands.Profile.Extensions.CancelRetryDelegatingHandler -ArgumentList [System.TimeSpan]::FromSeconds(10), 3
+[Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.ClientFactory.AddHandler($handler)
